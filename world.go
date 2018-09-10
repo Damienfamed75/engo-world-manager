ec52ceb02cf6834c78a200c7b3f5006777739e30
Damien <thedoubledimensions@gmail.com>
2018-09-10 16:32:25 -0500

    Added removing functionality

--- world.go
+++ world.go
@@ -9,10 +9,11 @@ import (
 
 // RemoveSystem will run through the world systems and remove
 // the desired system you passed along with your parameters
-func RemoveSystem(world *ecs.World, sysType interface{}, param ...interface{}) {
+func RemoveSystem(world *ecs.World, sysType interface{}, param ecs.BasicEntity) {
 	for _, system := range world.Systems() {
 		switch sys := reflect.TypeOf(system); sys {
 		case reflect.TypeOf(sysType):
+			system.Remove(param)
 			fmt.Println("REMOVED")
 		default:
 			fmt.Println("FAILED")
