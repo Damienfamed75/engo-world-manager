83f4e9749ec3a739c0786f129424bb747bc1a0e5
Damien <thedoubledimensions@gmail.com>
2018-09-10 16:06:11 -0500

    Initial push

---
+++ world.go
@@ -0,0 +1,19 @@
+package worldmanager
+
+import (
+	"fmt"
+	"reflect"
+
+	"engo.io/ecs"
+)
+
+// RemoveSystem will run through the world systems and remove
+// the desired system you passed along with your parameters
+func RemoveSystem(world *ecs.World, sysType interface{}, param ...interface{}) {
+	for _, system := range world.Systems() {
+		switch sys := reflect.TypeOf(system); sys {
+		case reflect.TypeOf(sysType):
+			fmt.Println("REMOVED")
+		}
+	}
+}
